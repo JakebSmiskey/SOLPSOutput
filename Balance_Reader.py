1a9f183f06b55799f7a8774f0227953d0a8ac283
JakebSmiskey <162725007+JakebSmiskey@users.noreply.github.com>
2025-03-03 13:19:38 -0500

    Update Balance_Reader.py

--- Balance_Reader.py
+++ Balance_Reader.py
@@ -38,6 +38,24 @@ class BalanceNC:
                 setattr(self,var_name,data)
             else:
                 setattr(self,var_name,data)
+        nx,ny,ns = np.shape(self.na)    
+        crx = np.reshape(self.crx,((nx*ny),1,4),order='F')
+        cry = np.reshape(self.cry,((nx*ny),1,4),order='F')
+        ll = np.concatenate((crx[:,:,0],cry[:,:,0]), axis = 1)
+        lr = np.concatenate((crx[:,:,1],cry[:,:,1]), axis = 1)
+        ul = np.concatenate((crx[:,:,2],cry[:,:,2]), axis = 1)
+        ur = np.concatenate((crx[:,:,3],cry[:,:,3]), axis = 1)
+        mesh_verts = [[tuple(lr[i]), tuple(ll[i]), tuple(ul[i]), tuple(ur[i])]
+                 for i in range(nx*ny)]
+        setattr(self, "nx",nx)
+        setattr(self, "ny",ny)
+        setattr(self, "ns",ns)
+        setattr(self, "lower_right_vertex", lr)
+        setattr(self, "lower_left_vertex", ll)
+        setattr(self, "upper_right_vertex", ul)
+        setattr(self, "upper_left_vertex", ur)
+        setattr(self, "mesh_verticies", mesh_verts)
+
 
 if __name__ == "__main__":
     test_balance = BalanceNC("balance.nc")
